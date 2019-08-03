d3b41c1dfc3ed92da6bf51e0db8dfdeae1c07f69
Paul Engel <pm_engel@icloud.com>
2019-08-04 00:56:55 +0200

    Add commands to manage upstreams and their domains

--- lib/vps.rb
+++ lib/vps.rb
@@ -9,8 +9,14 @@ module VPS
 
   def read_config(host, key = nil)
     if File.exists?(path = config_path(host))
-      config = YAML.load_file(path).with_indifferent_access
-      key ? config[key] : config
+      config = YAML.load_file(path)
+      with_indifferent_access(key ? config[key] : config)
+    elsif key.nil?
+      with_indifferent_access({
+        :user => nil,
+        :tool => nil,
+        :upstreams => []
+      })
     end
   end
 
@@ -39,6 +45,22 @@ module VPS
 
   def template(path)
     File.read("#{TEMPLATES}/#{path}")
+  end
+
+private
+
+  def with_indifferent_access(object)
+    case object
+    when Hash
+      object.inject({}.with_indifferent_access) do |hash, (key, value)|
+        hash[key] = with_indifferent_access(value)
+        hash
+      end
+    when Array
+      object.collect{|item| with_indifferent_access(item)}
+    else
+      object
+    end
   end
 
 end
--- lib/vps/cli.rb
+++ lib/vps/cli.rb
@@ -36,6 +36,83 @@ module VPS
       `open #{VPS.config_path(host)}`
     end
 
+    class Upstream < Thor
+      desc "add HOST PATH", "Add upstream to host configuration (option: --name)"
+      method_option :name
+      def add(host, path)
+        config = VPS.read_config(host)
+        config[:upstreams].push({
+          :name => options[:name] || File.basename(path),
+          :path => path,
+          :domains => []
+        })
+        VPS.write_config(host, config)
+      end
+
+      desc "remove HOST UPSTREAM", "Remove upstream from host configuration"
+      def remove(host, name)
+        config = VPS.read_config(host)
+        config[:upstreams].reject!{|upstream| upstream[:name] == name}
+        VPS.write_config(host, config)
+      end
+
+      desc "list HOST", "List upstreams of host configuration"
+      def list(host)
+        config = VPS.read_config(host)
+        upstreams = config[:upstreams].collect do |upstream|
+          "* #{upstream[:name]} (#{upstream[:path].gsub(Dir.home, "~")})"
+        end.sort
+        puts upstreams
+      end
+    end
+
+    class Domain < Thor
+      desc "add HOST:UPSTREAM DOMAIN", "Add domain to host upstream"
+      def add(host_and_upstream, domain)
+        return unless domain.match(/^https?:\/\/([a-z0-9\-]{2,}\.)+[a-z]{2,}$/)
+
+        host, name = host_and_upstream.split(":")
+        config = VPS.read_config(host)
+
+        if (upstream = config[:upstreams].detect{|upstream| upstream[:name] == name})
+          upstream[:domains].push(domain).uniq!
+        end
+
+        VPS.write_config(host, config)
+      end
+
+      desc "remove HOST:UPSTREAM DOMAIN", "Remove domain from host upstream"
+      def remove(host_and_upstream, domain)
+        host, name = host_and_upstream.split(":")
+        config = VPS.read_config(host)
+
+        if (upstream = config[:upstreams].detect{|upstream| upstream[:name] == name})
+          upstream[:domains].delete(domain)
+        end
+
+        VPS.write_config(host, config)
+      end
+
+      desc "list HOST[:UPSTREAM]", "List domains of host (:upstream optional)"
+      def list(host_and_optional_upstream)
+        host, name = host_and_optional_upstream.split(":")
+        config = VPS.read_config(host)
+
+        domains = config[:upstreams].collect do |upstream|
+          if name.nil? || upstream[:name] == name
+            upstream[:domains]
+              .collect{|domain| "  ~> #{domain}"}
+              .unshift("* #{upstream[:name]}:")
+          end
+        end.flatten.compact
+
+        puts domains
+      end
+    end
+
+    register(Upstream, "upstream", "upstream", "Manage host upstreams")
+    register(Domain, "domain", "domain", "Manage upstream domains")
+
     desc "-v, [--version]", "Show VPS version number"
     map %w(-v --version) => :version
     def version
